3419e67ab537e0b821231aa54f41cf439bc9caee
Benjamim <benjamimq015@gmail.com>
2021-03-04 18:26:11 -0400

    feat: add global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import GlobalStyle from '../styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default MyApp;
